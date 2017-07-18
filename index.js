8d8df0eb814f4255023a8e19c88949e963563d36
erik <erik.escoffier@gmail.com>
2017-07-18 13:05:06 +0200

    testing animation on Mapbox GL sandbox client

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ var map = new mapboxgl.Map({
   container: 'map',
   // style: 'mapbox://styles/mapbox/streets-v9'
   // style: 'mapbox://styles/danielcaso/cj49xom6535sn2spado3w5to6'
-  style: 'http://34.230.92.118:8080/styles/our-north-star/style.json'
+  style: 'http://localhost:8080/styles/vessels/style.json'
+});
+
+let t = 1000;
+
+const frame = () => {
+  const filters = ['>=', 't', t];
+  map.setFilter('vessels', filters);
+  t += 20;
+  requestAnimationFrame(frame);
+}
+
+map.on('load', () => {
+  // frame();
 });
