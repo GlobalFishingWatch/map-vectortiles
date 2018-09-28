91f3c91508ca7cd7fcb729e8f329eb3a844ac0f2
nerik <erik.escoffier@gmail.com>
2018-09-28 15:36:34 +0200

    used streamed JSON in points generator

--- points-generator/index.js
+++ points-generator/index.js
@@ -3,6 +3,7 @@ const execSync = require('child_process').execSync
 const turfinside = require('@turf/inside')
 const turfhelpers = require('@turf/helpers')
 const landmass = require('./landmass').features[0]
+var JSONStream = require( 'JSONStream' )
 
 const dataset = process.argv[2]
 const path = `./data/${dataset}`
@@ -19,22 +20,22 @@ if (isNaN(maxZoom)) {
 
 var DATE_START = new Date(2012,0,1).getTime()
 var DATE_END = new Date(2016,11,31).getTime()
+// var DATE_START = new Date(2010,0,1).getTime()
+// var DATE_END = new Date(2012,0,1).getTime()
 var dateInterval = DATE_END - DATE_START
-
-const geoJSON = {
-  'type': 'FeatureCollection',
-  'features': []
-}
 
 var i = 0
 
-while (geoJSON.features.length < numFeatures) {
+const points = []
+
+while (points.length < numFeatures) {
   var randomLat = (180 * Math.random()) - 90
   var randomLng = (360 * Math.random()) - 180
   var randomDatetime = DATE_START + Math.floor(dateInterval * Math.random())
   var pt = turfhelpers.point([randomLng, randomLat])
   if (turfinside(pt, landmass)) continue
-  geoJSON.features.push({
+  
+  points.push({
     'type': 'Feature',
     'properties': {
       datetime: randomDatetime,
@@ -53,9 +54,29 @@ while (geoJSON.features.length < numFeatures) {
 
 execSync(`rm -f ${path}/data/${dataset}.mbtiles`)
 
-fs.writeFileSync(`${path}/data/${dataset}.json`, JSON.stringify(geoJSON))
-// --drop-rate=0 : do not try to reduce the number of features at x levels below --maximum-zoom
-const tippecanoe = `tippecanoe -o ${path}/data/${dataset}.mbtiles -l ${dataset} --maximum-zoom=${maxZoom} --minimum-zoom=2 --no-feature-limit --no-tile-size-limit --drop-rate=0 ${path}/data/${dataset}.json`
-console.log(tippecanoe)
-const ex = execSync(tippecanoe)
-console.log(ex.toString())
+const transformStream = JSONStream.stringify('{"type":"FeatureCollection","features":[', '\n,\n', ']}')
+const outputStream = fs.createWriteStream(`${path}/data/${dataset}.json`)
+transformStream.pipe(outputStream)
+points.forEach((p, i) => { console.log(i); transformStream.write(p) })
+outputStream.on('error', (a) => {
+  console.log(a)
+})  
+transformStream.on('error', (a) => {
+  console.log(a)
+})  
+transformStream.on('end', () => {
+  console.log('end JSON stringify')
+})  
+outputStream.on('finish', () => {
+  console.log('JSON done')
+  // --drop-rate=0 : do not try to reduce the number of features at x levels below --maximum-zoom
+  const tippecanoe = `tippecanoe -o ${path}/data/${dataset}.mbtiles -l ${dataset} --maximum-zoom=${maxZoom} --minimum-zoom=2 --no-feature-limit --no-tile-size-limit --drop-rate=0 ${path}/data/${dataset}.json`
+  console.log(tippecanoe)
+  const ex = execSync(tippecanoe)
+  console.log(ex.toString())
+})
+transformStream.end()
+
+
+
+
